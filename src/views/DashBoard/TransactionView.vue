e30c3414ebd7422ea0bd36470872d1c63b12fd65
MAC <mac@MACs-MacBook-Pro.local>
2024-08-17 18:36:12 +0100

    twentyninth push

--- src/views/DashBoard/TransactionView.vue
+++ src/views/DashBoard/TransactionView.vue
@@ -264,6 +264,44 @@
                     </div>
                   </td>
                 </tr>
+                <tr >
+                  <td>
+                    <div class="separate-1">
+                      <p>
+                        <!--                    <i class='bx bx-log-in-circle' ></i>-->
+                        <i class='bx bx-log-in-circle pending' ></i>
+                        <!--                    <i class='bx bx-log-out-circle'  ></i>-->
+                      </p>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="separate-2">
+                      <div class="name-text">Cr</div>
+                      <!--                      <p class="text-block-76" v-if="items.transactionStatus === 'VERIFIED'">{{items.transactionStatus}}</p>-->
+                      <!--                      <p class="text-block-77" v-else>{{items.transactionStatus}}</p>-->
+                    </div>
+                  </td>
+                  <td>
+                    <div class="separate-2">
+                      <div class="name-text">{{this.contacts.LastName2}} {{this.contacts.FirstName2}}</div>
+                      <!--                      <p class="text-block-76" v-if="items.transactionStatus === 'VERIFIED'">{{items.transactionStatus}}</p>-->
+                      <!--                      <p class="text-block-77" v-else>{{items.transactionStatus}}</p>-->
+                    </div>
+                  </td>
+                  <td>
+                    <div class="separate-3">
+                      <p class="amount-text" >&#36; 2,000.00
+                        <!--                    <span v-if="items.accountNumber === accountCAD">CAD</span>-->
+                        <!--                    <span v-if="items.accountNumber === accountNaira">NG</span>-->
+                      </p>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="separate-3">
+                      <p class="amount-text">17th Aug 2024</p>
+                    </div>
+                  </td>
+                </tr>
                 </tbody>
 
               </table>
